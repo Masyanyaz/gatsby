f686f2082f01b9cba9526e59a307920d0e7831e6
Nomel <nomelnomel@gmail.com>
2020-06-23 02:02:28 +0300

    22.06

--- src/components/programs/filters.js
+++ src/components/programs/filters.js
@@ -1,11 +1,11 @@
 import React from "react"
-import { Link, useStaticQuery, graphql } from "gatsby"
+import {Link, useStaticQuery, graphql} from "gatsby"
 
 import './filters.css'
 
-const Filters = ({ types, pagePath }) => {
-
-  const data = useStaticQuery(graphql`
+const Filters = ({types, pagePath, categories}) => {
+    console.log(categories)
+    const data = useStaticQuery(graphql`
     query {
       allStrapiDirections(filter: {
           tours: {
@@ -27,44 +27,65 @@ const Filters = ({ types, pagePath }) => {
     }
   `)
 
-  return (
-    <div>
-      <div>
-        {
-          data.allStrapiDirections.edges.map(({ node: { id, name, path } }) => (
-            <Link
-              to={ `/catalogue/filters/${ path }/tours/all/` }
-              key={ id } style={ { marginRight: "10px" } }
-              activeClassName={'active'}
-            >
-              { name }
-            </Link>
-          ))
-        }
-      </div>
-      <div>
-        <Link
-          to={ `/catalogue/filters/${ pagePath }/tours/all/` }
-          style={ { marginRight: "10px" } }
-          activeClassName={'active'}
-        >
-          Все
-        </Link>
-        {
-          types.map(({ node: { id, name, path } }) => (
-            <Link
-              to={ `/catalogue/filters/${ pagePath }/tours/${ path }/` }
-              key={ id }
-              activeClassName={'active'}
-              style={ { marginRight: "10px" } }
-            >
-              { name }
-            </Link>
-          ))
-        }
-      </div>
-    </div>
-  )
+    return (
+        <div>
+            <div>
+                {
+                    data.allStrapiDirections.edges.map(({node: {id, name, path}}) => (
+                        <Link
+                            to={`/catalogue/filters/${path}/tours/all/`}
+                            key={id} style={{marginRight: "10px"}}
+                            activeClassName={'active'}
+                        >
+                            {name}
+                        </Link>
+                    ))
+                }
+            </div>
+            <div>
+                <Link
+                    to={`/catalogue/filters/${pagePath}/tours/all/`}
+                    style={{marginRight: "10px"}}
+                    activeClassName={'active'}
+                >
+                    Все
+                </Link>
+                {
+                    categories.map(({node: {id, name, path}}) => (
+                        <Link
+                            to={`/catalogue/filters/${pagePath}/tours/${path}/`}
+                            key={id}
+                            activeClassName={'active'}
+                            style={{marginRight: "10px"}}
+                        >
+                            {name}
+                        </Link>
+                    ))
+                }
+            </div>
+            <div>
+                <Link
+                    to={`/catalogue/filters/${pagePath}/tours/all/`}
+                    style={{marginRight: "10px"}}
+                    activeClassName={'active'}
+                >
+                    Все
+                </Link>
+                {
+                    types.map(({node: {id, name, path}}) => (
+                        <Link
+                            to={`/catalogue/filters/${pagePath}/tours/${path}/`}
+                            key={id}
+                            activeClassName={'active'}
+                            style={{marginRight: "10px"}}
+                        >
+                            {name}
+                        </Link>
+                    ))
+                }
+            </div>
+        </div>
+    )
 }
 
 export default Filters
