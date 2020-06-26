dff4fd60916455a0a8dddd56898a0433525ccadf
Nomel <nomelnomel@gmail.com>
2020-06-26 16:54:14 +0300

    fixed menu

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,39 +3,45 @@ import PropTypes from "prop-types"
 import React from "react"
 import "./header-menu.css"
 
-const Header = ({ siteTitle }) => (
-  <header>
-    <div className="header__menu">
-      <div className="header__menu-left-side">
-        <Link to="#" className="header__menu-left-side-element" id="hmlse__left"> Destinations </Link>
-        <Link to="#" className="header__menu-left-side-element" id="hmlse__center" >Inspirations</Link>
-        <Link to="#" className="header__menu-left-side-element" id="hmlse__right"> Services </Link>
-      </div>
-      <div className="header__menu-center">
-        <Link to="/">
-          <img src="https://cuisinedevoyage.com/img/logo-v2t.svg" alt="" />
-        </Link>
-        <p>Voyagez a votre gout!</p>
-      </div>
-      <div className="header__menu-right-side">
-        <div className="header__menu-right-side-top">
-          <Link to="#">
-            <img src="https://cuisinedevoyage.com/img/mail.svg" alt="" />
-          </Link>
-          <Link to="#">
-            <img src="https://cuisinedevoyage.com/img/phone.svg" id="phone" alt="" />
-          </Link>
-          <Link to="#" className="header__menu-right-side-top-button">envoyer une demande</Link>
+
+
+const Header = ({ siteTitle }) => {
+  const headerClass = (window.pageYOffset < 100) ? 'header__menu' : 'header__menu scrolled'
+  return(
+      <header>
+
+        <div className={headerClass}>
+          <div className="header__menu-left-side">
+            <Link to="#" className="header__menu-left-side-element" id="hmlse__left"> Destinations </Link>
+            <Link to="#" className="header__menu-left-side-element" id="hmlse__center" >Inspirations</Link>
+            <Link to="#" className="header__menu-left-side-element" id="hmlse__right"> Services </Link>
+          </div>
+          <div className="header__menu-center">
+            <Link to="/">
+              <img src="https://cuisinedevoyage.com/img/logo-v2t.svg" alt="" />
+            </Link>
+            <p>Voyagez a votre gout!</p>
+          </div>
+          <div className="header__menu-right-side">
+            <div className="header__menu-right-side-top">
+              <Link to="#">
+                <img src="https://cuisinedevoyage.com/img/mail.svg" alt="" />
+              </Link>
+              <Link to="#">
+                <img src="https://cuisinedevoyage.com/img/phone.svg" id="phone" alt="" />
+              </Link>
+              <Link to="#" className="header__menu-right-side-top-button">envoyer une demande</Link>
+            </div>
+            <div className="header__menu-right-side-bottom">
+              <Link to="#" className="header__menu-right-side-bottom-element">Qui sommes-nous</Link>
+              <Link to="#" className="header__menu-right-side-bottom-element">Nouvelles</Link>
+              <Link to="#" className="header__menu-right-side-bottom-element">Contacts</Link>
+            </div>
+          </div>
         </div>
-        <div className="header__menu-right-side-bottom">
-          <Link to="#" className="header__menu-right-side-bottom-element">Qui sommes-nous</Link>
-          <Link to="#" className="header__menu-right-side-bottom-element">Nouvelles</Link>
-          <Link to="#" className="header__menu-right-side-bottom-element">Contacts</Link>
-        </div>
-      </div>
-    </div>
-  </header>
-)
+      </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string
